d933134eb79ce5d420b0dd33325919b20448f962
kenshak <phylace0@gmail.com>
2021-08-13 16:54:40 +0200

    'mongoose-create'

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,18 @@
 import { Injectable, NotFoundException } from "@nestjs/common"
+import { InjectModel } from "@nestjs/mongoose"
+import { Model } from "mongoose"
 import {Product} from './product.model'
 
 @Injectable()
 export class ProductsService{
     private products: Product[] = []
+   
+    constructor(@InjectModel('Product') private readonly productModel: Model<Product>){}
+
     insertProduct(title:string, desc:string, price:number){
-        const prodId = Math.random().toString()
-        const newProduct = new Product(prodId, title, desc, price)
-        this.products.push(newProduct)
-        return prodId
+        const newProduct = new this.productModel({title, description:desc, price})
+         newProduct.save()
+        return newProduct
     }
     getProducts(){
         return [...this.products]
