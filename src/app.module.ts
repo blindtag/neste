2edd1eb7b6d27c9d353d8ef9df85059230ff17ed
kenshak <phylace0@gmail.com>
2021-08-13 16:42:01 +0200

    'mongoose'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule} from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/product.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule, 
+    MongooseModule.forRoot('mongodb+srv://ken123:ken123@cluster0.ughez.mongodb.net/neste?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
